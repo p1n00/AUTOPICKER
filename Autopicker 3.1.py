e26babc98e983ce86c4085381f93f09743a9d466
p1n00 <93547806+p1n00@users.noreply.github.com>
2021-11-12 08:41:48 +0100

    Add files via upload

---
+++ Autopicker 3.1.py
@@ -0,0 +1,370 @@
+#VALORANT AUTOPICKER BY p1n00
+
+#IMPORTAT LIBRERIAS
+import pyautogui
+import keyboard
+import tkinter
+from tkinter import Label, Tk
+
+ventana = Tk()
+
+ventana.title("AUTOPICKER BY p1n00")
+def kayo():
+    keyboard.record(until="alt gr")
+
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xk = 826
+    y1 = 933
+#PICKEO DE KAYO
+    pyautogui.moveTo(xk,y1)
+    pyautogui.click(xk,y1)
+    pyautogui.click(xk,y1)
+    pyautogui.click(xk,y1)
+
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def jett():
+    keyboard.record(until="alt gr")
+
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xj = 760
+    y1 = 933
+#PICKEO DE JETT
+    pyautogui.moveTo(xj,y1)
+    pyautogui.click(xj,y1)
+    pyautogui.click(xj,y1)
+    pyautogui.click(xj,y1)
+
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def sage():
+    keyboard.record(until="alt gr")
+
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xs = 1108
+    y1 = 933
+
+#PICKEO DE SAGE
+    pyautogui.moveTo(xs,y1)
+    pyautogui.click(xs,y1)
+    pyautogui.click(xs,y1)
+    pyautogui.click(xs,y1)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    
+def cypher():
+    keyboard.record(until="alt gr")
+
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xc = 674
+    y1 = 933
+
+#PICKEO DE CYPHER
+    pyautogui.moveTo(xc,y1)
+    pyautogui.click(xc,y1)
+    pyautogui.click(xc,y1)
+    pyautogui.click(xc,y1)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def omen():
+    keyboard.record(until="alt gr")
+
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xo = 913
+    y1 = 933
+
+#PICKEO DE OMEN
+    pyautogui.moveTo(xo,y1)
+    pyautogui.click(xo,y1)
+    pyautogui.click(xo,y1)
+    pyautogui.click(xo,y1)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def phenix():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xp = 1004
+    y1 = 933
+
+#PICKEO DE PHENIX
+    pyautogui.moveTo(xp,y1)
+    pyautogui.click(xp,y1)
+    pyautogui.click(xp,y1)
+    pyautogui.click(xp,y1)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def brimstone():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xb = 586
+    y1 = 933
+
+#PICKEO DE BRIMSTONE
+    pyautogui.moveTo(xb,y1)
+    pyautogui.click(xb,y1)
+    pyautogui.click(xb,y1)
+    pyautogui.click(xb,y1)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def sova():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xsv = 1152
+    y1 = 933
+
+#PICKEO DE SOVA
+    pyautogui.moveTo(xsv,y1)
+    pyautogui.click(xsv,y1)
+    pyautogui.click(xsv,y1)
+    pyautogui.click(xsv,y1)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def astra():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xa = 1257
+    y1 = 933
+
+#PICKEO DE ASTRA
+    pyautogui.moveTo(xa,y1)
+    pyautogui.click(xa,y1)
+    pyautogui.click(xa,y1)
+    pyautogui.click(xa,y1)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def breach():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xb = 1337
+    y1 = 933
+
+#PICKEO DE 
+    pyautogui.moveTo(xb,y1)
+    pyautogui.click(xb,y1)
+    pyautogui.click(xb,y1)
+    pyautogui.click(xb,y1)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def killjoy():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xki = 581
+    y2 = 1020
+
+#PICKEO DE 
+    pyautogui.moveTo(xki,y2)
+    pyautogui.click(xki,y2)
+    pyautogui.click(xki,y2)
+    pyautogui.click(xki,y2)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def raze():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xr = 675
+    y2 = 1020
+
+#PICKEO DE 
+    pyautogui.moveTo(xr,y2)
+    pyautogui.click(xr,y2)
+    pyautogui.click(xr,y2)
+    pyautogui.click(xr,y2)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def reyna():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xre = 739
+    y2 = 1020
+
+#PICKEO DE 
+    pyautogui.moveTo(xre,y2)
+    pyautogui.click(xre,y2)
+    pyautogui.click(xre,y2)
+    pyautogui.click(xre,y2)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def skye():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xsk = 843
+    y2 = 1020
+
+#PICKEO DE 
+    pyautogui.moveTo(xsk,y2)
+    pyautogui.click(xsk,y2)
+    pyautogui.click(xsk,y2)
+    pyautogui.click(xsk,y2)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def viper():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xv = 885
+    y2 = 1020
+
+#PICKEO DE 
+    pyautogui.moveTo(xv,y2)
+    pyautogui.click(xv,y2)
+    pyautogui.click(xv,y2)
+    pyautogui.click(xv,y2)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+def yoru():
+    keyboard.record(until="alt gr")
+#MOVER A ESQUINA PARA CALIBRAR
+    pyautogui.moveTo(1,1)
+    xy = 999
+    y2 = 1020
+
+#PICKEO DE 
+    pyautogui.moveTo(xy,y2)
+    pyautogui.click(xy,y2)
+    pyautogui.click(xy,y2)
+    pyautogui.click(xy,y2)
+    
+#CONFIRMAR
+    pyautogui.moveTo(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+    pyautogui.click(979,807)
+
+titulo = tkinter.Label(text=("Selecciona agente y presiona ALT GR cuando quieras que se pickee"))
+titulo.pack(side="top")
+
+boton_brimstone = tkinter.Button(ventana, text="BRIMSTONE", command=brimstone)
+boton_brimstone.pack(side="left")
+
+boton_cypher = tkinter.Button(ventana, text="CYPHER", command=cypher)
+boton_cypher.pack(side="left")
+
+boton_jett = tkinter.Button(ventana, text="JETT", command=jett)
+boton_jett.pack(side="left")
+
+boton_kayo = tkinter.Button(ventana, text="KAYO", command=kayo)
+boton_kayo.pack(side="left")
+
+boton_omen = tkinter.Button(ventana, text="OMEN", command=omen)
+boton_omen.pack(side="left")
+
+boton_phenix = tkinter.Button(ventana, text="PHENIX", command=phenix)
+boton_phenix.pack(side="left")
+
+boton_sage = tkinter.Button(ventana, text="SAGE", command=sage)
+boton_sage.pack(side="left")
+
+boton_sova = tkinter.Button(ventana, text="SOVA", command=sova)
+boton_sova.pack(side="left")
+
+boton_astra = tkinter.Button(ventana, text="ASTRA", command=astra)
+boton_astra.pack(side="left")
+
+boton_breach = tkinter.Button(ventana, text="BREACH", command=breach)
+boton_breach.pack(side="left")
+
+boton_killjoy = tkinter.Button(ventana, text="KILLJOY", command=killjoy)
+boton_killjoy.pack(side="left")
+
+boton_raze = tkinter.Button(ventana, text="RAZE", command=raze)
+boton_raze.pack(side="left")
+
+boton_reyna = tkinter.Button(ventana, text="REYNA", command=reyna)
+boton_reyna.pack(side="left")
+
+boton_skye = tkinter.Button(ventana, text="SKYE", command=skye)
+boton_skye.pack(side="left")
+
+boton_viper = tkinter.Button(ventana, text="VIPER", command=viper)
+boton_viper.pack(side="left")
+
+boton_yoru = tkinter.Button(ventana, text="YORU", command=yoru)
+boton_yoru.pack(side="left")
+
+ventana.mainloop()
